5e5a278a5e51e530c783895b3b45b8c3d6e85d89
Pranay R <rps2579@gmail.com>
2022-07-16 20:49:39 +0530

    use callback to combine both API calls

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,27 +1,44 @@
-const request = require('request');
+const request = require("request");
 
-const url = 'http://api.weatherstack.com/current?access_key=57616d1b5eee9836067ee022fd83359a&query=17.6868,83.2185';
+const fetch_weather_data = (lat, lon) => {
+  const weather_url = `http://api.weatherstack.com/current?access_key=57616d1b5eee9836067ee022fd83359a&query=${lat},${lon}`;
 
-request({ url: url, json: true }, (error, response) => {
+  request({ url: weather_url, json: true }, (error, response) => {
     if (error) {
-        console.log('Unable to connect to weather service');
+      console.log("Unable to connect to weather service");
     } else if (response.body.error) {
-        console.log('Unable to find location');
+      console.log("Unable to find location");
     } else {
-        console.log("It is currently ", response.body.current.temperature, " degrees out. There is a ", response.body.current.precip*100, "% chance of rain.");
-        console.log(response.body.current.weather_descriptions[0]);
+      console.log(
+        "It is currently ",
+        response.body.current.temperature,
+        " degrees out. There is a ",
+        response.body.current.precip * 100,
+        "% chance of rain."
+      );
+      console.log(response.body.current.weather_descriptions[0]);
     }
-});
+  });
+};
 
-const location_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Yarada.json?access_token=pk.eyJ1IjoicnBzMjU3OSIsImEiOiJjbDVuejF0c2swYzNzM2ZtandycXU4bTh0In0.vhJoOvO-XF_AE-H_x94hrQ&limit=1';
-
-request({ url: location_url, json: true }, (error, response) => {
+const fetch_location_data = (address, callback) => {
+  const location_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/
+    ${encodeURIComponent(address)}
+    .json?access_token=pk.eyJ1IjoicnBzMjU3OSIsImEiOiJjbDVuejF0c2swYzNzM2ZtandycXU4bTh0In0.vhJoOvO-XF_AE-H_x94hrQ&limit=1`;
+  
+    request({ url: location_url, json: true }, (error, response) => {
     if (error) {
-        console.log('Unable to connect to location service');
+      console.log("Unable to connect to location service");
     } else if (response.body.features.length === 0) {
-        console.log('Unable to find location');
+      console.log("Unable to find location");
     } else {
-        console.log(response.body.features[0].center);
-        console.log(response.body.features[0].center[1], response.body.features[0].center[0]);
+      console.log(response.body.features[0].center);
+      const lat = response.body.features[0].center[1];
+      const lon = response.body.features[0].center[0];
+
+      callback(lat, lon);
     }
-});
+  });
+};
+
+fetch_location_data('Gajuwaka', fetch_weather_data);
