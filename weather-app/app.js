e10def170f1d965b0df62cb841f70e69e961228b
Pranay R <rps2579@gmail.com>
2022-07-16 19:32:40 +0530

    Use JSON option of request

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,13 +2,12 @@ const request = require('request');
 
 const url = 'http://api.weatherstack.com/current?access_key=57616d1b5eee9836067ee022fd83359a&query=17.6868,83.2185';
 
-request({ url: url }, (error, response) => {
+request({ url: url, json: true }, (error, response) => {
     if (error) {
         console.log('Unable to connect to weather service');
     } else if (response.body.error) {
         console.log('Unable to find location');
     } else {
-        const data = JSON.parse(response.body);
-        console.log(data.current);
+        console.log("It is currently ", response.body.current.temperature, " degrees out. There is a ", response.body.current.precip*100, "% chance of rain.");
     }
 });
