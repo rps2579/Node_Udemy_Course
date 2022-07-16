292b5f02499d2b6fcebacda4b2ab38e3319e4f9a
Pranay R <rps2579@gmail.com>
2022-07-16 22:19:37 +0530

    using both responses in nested callback

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,16 +1,17 @@
 const fetch_location_data = require("./utils/geocode");
 const fetch_weather_data = require("./utils/weatherstack");
 
-fetch_location_data("Gajuwaka", (error, response) => {
+fetch_location_data("Gajuwaka", (error, locationData) => {
   if (error) {
-    console.log(error);
-  } else {
-    fetch_weather_data(response, (error, response) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(response);
-      }
-    });
+    return console.log(error);
   }
+
+  fetch_weather_data(locationData, (error, weatherData) => {
+    if (error) {
+      return console.log(error);
+    }
+    
+    // we can use both the locationData and weatherData here
+    console.log(weatherData);
+  });
 });
